ac4429db9b8e9e6ba243e58e9c44c4445c29dcc0
AnnaK87 <anna.kaukh@gmail.com>
2018-11-20 19:14:53 +0400

    initial commit

---
+++ CoderSwag/CoderSwag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoderSwag
+//
+//  Created by Anna Kaukh on 11/20/18.
+//  Copyright © 2018 Anna Kaukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
